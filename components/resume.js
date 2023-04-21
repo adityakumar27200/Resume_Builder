6cce89367b0be517cb02066b842c982febaba343
Nischal <gupta.nischal014@gmail.com>
2023-04-21 16:53:30 +0530

    resume component

---
+++ components/resume.js
@@ -0,0 +1,18 @@
+export default function Resume() {
+  return (
+    <>
+      <div className="container">
+        <div className="resumeContainer">
+          <div className="topCard">
+            <h1 className="name">Nischal Gupta</h1>
+            <h1 className="role">Developer</h1>
+            <p className="brief">
+              Transforming complex problems into elegant software solutions,
+              powered by cutting-edge technology and unparalleled creativity
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
